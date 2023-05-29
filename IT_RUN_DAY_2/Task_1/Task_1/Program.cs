7d1bb1d0e7b6b401d9bcf18965f27aa243c70234
QulmatovBehzod02 <bekzodbek.tjk.001@gmail.com>
2023-05-29 11:30:23 +0500

    first task completed

--- IT_RUN_DAY_2/Task_1/Task_1/Program.cs
+++ IT_RUN_DAY_2/Task_1/Task_1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+    int intfive = 5;//Число 5 хранится в stack
+    object objFive = intfive;//Число 5 хранится в heap
+
+int intFive2 = 5;//Число 5 хранится в stack
+object objFive2 = objFive;//Число 5 хранится в heap
+
+bool isIntEqual = intfive == intFive2;
+    Console.WriteLine(isIntEqual);//Здесь будет True, потому что 5 ровно 5 
+
+    bool isObjEqual = objFive == objFive2;
+    Console.WriteLine(isObjEqual);//Здесь будет False, потому что 
