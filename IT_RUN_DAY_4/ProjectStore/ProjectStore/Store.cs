e6af69dc3cbae64f741bd8a6d809109299d7018f
Khusrav-Muhammadi <khusravmuhammadi.73>
2023-06-01 19:55:22 +0500

    added task 4

---
+++ IT_RUN_DAY_4/ProjectStore/ProjectStore/Store.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Product
+{
+    public class StoreClass
+    {
+        string Name { get; set; }
+        int Count { get; set; }
+
+        public StoreClass() { }
+
+        public StoreClass(string name, int count)
+        {
+            Name = name;
+            Count = count;
+        }
+
+        public void SaveToFile( string name, int count)
+        { 
+            
+            using (StreamWriter sw = new StreamWriter("Store.txt", true))
+             {
+                sw.WriteLine($"{name} {count}");
+                sw.Flush();
+             }
+
+             Console.WriteLine("Successfully added");
+            
+        }
+
+        public static string[] GetProducts(string path)
+        {
+            string[] products = File.ReadAllLines(path, Encoding.UTF8) ;
+            return products ;
+        }
+
+        public static bool ProductExists(string name, string path)
+        {
+            bool exists = false ;
+            string[] products = File.ReadAllLines(path, Encoding.UTF8);
+            for (int i = 0; i < products.Length; i++)
+            {
+                string product = products[i];
+                var splitted = product.Split(' ');
+
+                string productName = splitted[0];
+                int countOfProduct = int.Parse(splitted[1]);
+                if (productName == name)
+                {
+                    exists= true;
+                }
+            }
+            return exists ;
+        }
+
+    }
+}
